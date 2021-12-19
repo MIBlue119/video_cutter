e44e9a59e719d10678be2032ea6dd1460911d87f
MIBlue119 <miblue119@gmail.com>
2021-12-19 12:06:56 +0800

    Feat: add the file selected handler and import useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+    const [selectedFile, setSelectedFile] = useState();
+    const [isFilePicked, setIsFilePicked] = useState(false);
+
+    // Define the file selected handler
+    const selectedFileHandler = (event) => {
+        const selectedFileFromClient = event.target.files[0];
+        selectedFile(selectedFileFromClient);
+        selectedFileFromClient(true);
+    };
 
 
     return (
-        <div>
-            Hello
+        <div className="ui container">
+            <div className="ui label">
+                <h1>Video Cutter</h1>
+            </div>
+            <br />
+            <label htmlFor="file">Upload your media(video/audio)</label>
+
+            <input
+                className="ui input"
+                type="file"
+                onChange={selectedFileHandler}
+                accept="audio/*,video/*"
+            />
         </div>
     );
 };
